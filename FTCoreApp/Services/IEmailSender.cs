432e7642d3db409482da05f58a34a1ed0eab3ade
29099 <Datnp5@gmail.com>
2024-06-20 02:39:08 +0700

    Add project files.

---
+++ FTCoreApp/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FTCoreApp.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
