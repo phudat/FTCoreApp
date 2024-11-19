8e1743166a00132c096844f3585cc5873c8d35ca
29099 <datnp5@fpt.com>
2024-11-19 14:20:25 +0700

    [change]

--- test1.cs
+++ test1.cs
@@ -1,1 +1,22 @@
-1.2
+var totalRecordsToSync = listOrderReturnWillBeSync.Count;
+  
+var tasks = new List<Task>();
+var semaphore = new SemaphoreSlim(maxParallelTasks); 
+
+for (var i = 0; i < totalRecordsToSync; i += batchSize)
+{
+    var batchOrderReturnCodes = listOrderReturnWillBeSync.Skip(i).Take(batchSize).ToList();
+    await semaphore.WaitAsync();
+    tasks.Add(Task.Run(async () =>
+    {
+        try
+        {
+            await ProcessBatch(batchOrderReturnCodes);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }));
+}
+await Task.WhenAll(tasks);
